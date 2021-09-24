8399c84048743b681b640b9bdfc29827048fd105
jitu712 <jitu712@gmail.com>
2021-09-24 09:47:55 +0530

    Revert "getter/setter instead of lombok"
    This reverts commit 98810357c52cdaff788c1bb86a713935b2c5b0b9.

--- src/main/java/com/example/springredditclone/dto/SubRedditDto.java
+++ src/main/java/com/example/springredditclone/dto/SubRedditDto.java
@@ -5,56 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
-// @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SubRedditDto {
     private Long id;
-
-    public SubRedditDto(Long id, String name, String description, Integer numberOfPosts) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.numberOfPosts = numberOfPosts;
-    }
-
-    public SubRedditDto() {
-    }
-
     private String name;
     private String description;
     private Integer numberOfPosts;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Integer getNumberOfPosts() {
-        return numberOfPosts;
-    }
-
-    public void setNumberOfPosts(Integer numberOfPosts) {
-        this.numberOfPosts = numberOfPosts;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
